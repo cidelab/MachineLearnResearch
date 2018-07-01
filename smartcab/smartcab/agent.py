931dde9c30e4b01c146f763e2ac901ccbfbcf69c
cidelab <cidelab@gmail.com>
2018-07-01 11:50:17 +0100

    smartcab update

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -167,7 +167,7 @@ def run():
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent)
+    env.set_primary_agent(agent, enforce_deadline=True)
 
     ##############
     # Create the simulation
@@ -176,7 +176,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,update_delay=0.01, log_metrics=True)
+    sim = Simulator(env,update_delay=0.1, log_metrics=True)
     
     ##############
     # Run the simulator
